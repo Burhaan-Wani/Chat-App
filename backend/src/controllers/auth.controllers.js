6cb27a747104606b3b2b0e7e39a65ef3f482b8d5
Burhaan-U-Deen wani <burhaanwani823@gmail.com>
2024-12-29 01:22:02 +0530

    add signup endpoint

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,15 +1,34 @@
 import AppError from "../lib/AppError.js";
 import catchAsync from "../lib/catchAsync.js";
+import { generateToken } from "../lib/jwtTokenAndCookie.js";
 import { signupSchema } from "../lib/zod/signupSchema.js";
+import User from "../models/user.model.js";
 
 // signup
-export const signup = catchAsync((req, res, next) => {
+export const signup = catchAsync(async (req, res, next) => {
   const { success, error } = signupSchema.safeParse(req.body);
   if (!success) {
     const err = error.errors.map(error => error.message).join(". ");
     return next(new AppError(err, 400));
   }
-  res.send("hi");
+
+  const { fullName, email, password } = req.body;
+  const existsUser = await User.findOne({ email });
+  if (existsUser) {
+    return next(new AppError("User with this email already exists", 400));
+  }
+
+  const newUser = await User.create({ fullName, email, password });
+
+  // generate JWT token and send as cookie
+  generateToken(newUser._id, res);
+  const { password: pass, ...user } = newUser._doc;
+  res.status(201).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
 });
 
 // login
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,6 +26,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
