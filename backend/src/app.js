a47939ee624a72b07992434e14de2a4449866571
Burhaan-U-Deen wani <burhaanwani823@gmail.com>
2024-12-31 23:12:42 +0530

    final commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,13 +25,6 @@ app.use(
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
 
-// Unhandled routes
-app.all("*", (req, _, next) => {
-  return next(
-    new AppError(`Can't find ${req.originalUrl} on this server!`, 404)
-  );
-});
-
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname, "../../frontend/dist"));
@@ -40,6 +33,12 @@ if (process.env.NODE_ENV === "production") {
     res.send(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
+// Unhandled routes
+app.all("*", (req, _, next) => {
+  return next(
+    new AppError(`Can't find ${req.originalUrl} on this server!`, 404)
+  );
+});
 
 // Error Handling Middleware
 app.use(globalErrorHandlingMiddleware);
